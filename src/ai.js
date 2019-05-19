c92fdbe581b3f86aedb8fdd873dd2e3cccb28d46
Bird <40967626+SuperSirBird@users.noreply.github.com>
2019-05-19 18:08:32 +1000

    Update ai.js

--- src/ai.js
+++ src/ai.js
@@ -1,6 +1,41 @@
 // Ai.js
 
-var s_weights = []
-var s_bias = []
+var s_weights = [];
+var s_bias = [];
+var s_pos = [];
+
+var n_pos = [];
+var n_value = [];
+
+var layers = 0;
+var layeramount = [];
 
 var ai_key = "U3VwZXJTaXJCaXJk";
+
+function createinputs(a) {
+  layeramount.push(a);
+  layers += 1;
+  
+  for (var i = 0; i < a; i++) {
+    n_pos.push("L"+layers+"P"+(i+1));
+    n_value.push(0);
+  }
+}
+
+function createlayer(a) {
+  layeramount.push(a);
+  layers += 1
+  
+  for (var i = 0; i < a; i++) {
+    n_pos.push("L"+layers+"P"+(i+1));
+    n_value.push(0);
+    for (var w = 0; w < layeramount[layers-1]; w++) {
+      spos.push("fL"+(layers-1)+"P"+(w+1)+"tL"+layers+"P"+(i+1));
+      s_weights.push(0.5);
+      s_bias.push(0);
+    }
+  }
+}
+
+createinputs(4);
+createlayer(5);
