130a0b2ee5211259a378fc76ab3ad030702421ac
Bird <40967626+SuperSirBird@users.noreply.github.com>
2019-05-19 23:09:48 +1000

    Update ai.js

--- src/ai.js
+++ src/ai.js
@@ -1,7 +1,7 @@
 // ai.js
 
 var c = document.getElementById("myCanvas");
-var ctx=c.getContext("2d");
+var ctx = c.getContext("2d");
 c.width  = window.innerWidth;
 c.height = window.innerHeight;
 
@@ -18,37 +18,35 @@ var layeramount = [];
 var ai_key = "U3VwZXJTaXJCaXJk";
 
 function createinputs(a) {
-  try {
   layeramount.push(a);
   layers += 1;
   
   for (var i = 0; i < a; i++) {
     n_pos.push("L"+layers+"P"+(i+1));
     n_value.push(0);
-  }} catch(err) {alert(err)}
+  }
 }
 
 function createlayer(a) {
-  try {
+  
   layeramount.push(a);
   layers += 1
   
   for (var i = 0; i < a; i++) {
     n_pos.push("L"+layers+"P"+(i+1));
     n_value.push(0);
-    for (var w = 0; w < layeramount[layers-1]; w++) {
+    for (var w = 0; w < layeramount[layers-2]; w++) {
       s_pos.push("fL"+(layers-1)+"P"+(w+1)+"tL"+layers+"P"+(i+1));
       s_weights.push(0.5);
       s_bias.push(0);
     }
-  }} catch(err) {alert(err)}
+  }
 }
-try{
+
 createinputs(4);
-createlayer(5);} catch(err) {alert(err)}
+createlayer(5);
 
 function drawUI() {
-  try { 
   for (var i = 0; i < 4; i++) {
     ctx.beginPath();
     //ctx.lineWidth = "6";
@@ -58,13 +56,11 @@ function drawUI() {
     ctx.fill();
     ctx.closePath();
   }
-  } catch(err) {alert(err)}
 }
 
 function step() {
-  try {
   drawUI();
-  window.requestAnimationFrame(step);} catch(err) {alert(err)}
+  window.requestAnimationFrame(step);
 }
-try{
-window.requestAnimationFrame(step);} catch(err) {alert(err)}
+
+window.requestAnimationFrame(step);
